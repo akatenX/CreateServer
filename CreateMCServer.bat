3a9325919374764801aeb3be21bf7fe18e7d1d3b
akatenX <88500948+akatenX@users.noreply.github.com>
2022-08-21 15:19:37 +0900

    Add files via upload

--- CreateMCServer.bat
+++ CreateMCServer.bat
@@ -171,22 +171,398 @@ if /i {%ANSWER%}=={4} (EXIT)
   echo "░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝"
   echo "-----------------------------------------------------"
   echo ***********************
-  echo 1. 1.17
-  echo 2. 1.18
-  echo 3. 1.19
-  echo 4. 戻る
+  echo 1. 1.8
+  echo 2. 1.9
+  echo 3. 1.10
+  echo 4. 1.11
+  echo 5. 1.12
+  echo 6. 1.13
+  echo 7. 1.14
+  echo 8. 1.15
+  echo 9. 1.16
+  echo 10. 1.17
+  echo 11. 1.18
+  echo 12. 1.19
+  echo 13. 戻る
   echo ***********************
-  SET /P ANSWER="番号を入力してください。(1,2,3,4)？ "
-  
+  SET /P ANSWER="番号を入力してください。(1~13)？ "
+
+
+
+
+
+
   if /i {%ANSWER%}=={1} (goto :no1)
   if /i {%ANSWER%}=={2} (goto :no2)
   if /i {%ANSWER%}=={3} (goto :no3)
-  if /i {%ANSWER%}=={4} (goto :run)
+  if /i {%ANSWER%}=={4} (goto :no4)
+  if /i {%ANSWER%}=={5} (goto :no5)
+  if /i {%ANSWER%}=={6} (goto :no6)
+  if /i {%ANSWER%}=={7} (goto :no7)
+  if /i {%ANSWER%}=={8} (goto :no8)
+  if /i {%ANSWER%}=={9} (goto :no9)
+  if /i {%ANSWER%}=={10} (goto :no10)
+  if /i {%ANSWER%}=={11} (goto :no11)
+  if /i {%ANSWER%}=={12} (goto :no12)
+  if /i {%ANSWER%}=={13} (goto :run)
   
 
 
-
   :no1
+    java -jar BuildTools.jar --rev 1.8
+    ren spigot-1.8.jar 1.8.jar
+    java -Xms1024M -Xmx1024M -jar 1.8.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.8.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.8.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+
+  :no2
+    java -jar BuildTools.jar --rev 1.9
+    ren spigot-1.9.jar 1.9.jar
+    java -Xms1024M -Xmx1024M -jar 1.9.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.9.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.9.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+
+
+  :no3
+    java -jar BuildTools.jar --rev 1.10
+    ren spigot-1.10.jar 1.10.jar
+    java -Xms1024M -Xmx1024M -jar 1.10.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.10.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.10.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+
+  :no4
+    java -jar BuildTools.jar --rev 1.11
+    ren spigot-1.11.jar 1.11.jar
+    java -Xms1024M -Xmx1024M -jar 1.11.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.11.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.11.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+  :no5
+    java -jar BuildTools.jar --rev 1.12
+    ren spigot-1.12.jar 1.12.jar
+    java -Xms1024M -Xmx1024M -jar 1.12.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.12.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.12.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+  :no6
+    java -jar BuildTools.jar --rev 1.13
+    ren spigot-1.13.jar 1.13.jar
+    java -Xms1024M -Xmx1024M -jar 1.13.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.13.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.13.jar nogui
+    pause
+    exit
+
+
+
+
+
+  :no7
+    java -jar BuildTools.jar --rev 1.14
+    ren spigot-1.14.jar 1.14.jar
+    java -Xms1024M -Xmx1024M -jar 1.14.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.14.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.14.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+  :no8
+    java -jar BuildTools.jar --rev 1.15
+    ren spigot-1.15.jar 1.15.jar
+    java -Xms1024M -Xmx1024M -jar 1.15.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.15.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.15.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+  :no9
+    java -jar BuildTools.jar --rev 1.16
+    ren spigot-1.16.jar 1.16.jar
+    java -Xms1024M -Xmx1024M -jar 1.16.jar nogui
+    @echo off
+    chcp 65001
+    SET /P ANSWER="eulaに同意しますか (Y/N)？"
+    
+    if /i {%ANSWER%}=={y} (goto :yes)
+    if /i {%ANSWER%}=={yes} (goto :yes)
+    
+    EXIT
+    
+    
+    :yes
+    @echo on
+    ren eula.txt eul.txt
+    set BEFORE_STRING=false
+    set AFTER_STRING=true
+    
+    set INPUT_FILE=eul.txt
+    set OUTPUT_FILE=eula.txt
+  
+    setlocal enabledelayedexpansion
+    for /f "delims=" %%a in (%INPUT_FILE%) do (
+    set line=%%a
+    echo !line:%BEFORE_STRING%=%AFTER_STRING%!>>%OUTPUT_FILE%
+    )
+    del /q eul.txt
+    echo java -Xms1024M -Xmx1024M -jar 1.16.jar nogui > serverstart.bat
+    java -Xms1024M -Xmx1024M -jar 1.16.jar nogui
+    pause
+    exit
+
+
+
+
+
+
+  :no10
     java -jar BuildTools.jar --rev 1.17
     ren spigot-1.17.jar 1.17.jar
     java -Xms1024M -Xmx1024M -jar 1.17.jar nogui
@@ -221,7 +597,7 @@ if /i {%ANSWER%}=={4} (EXIT)
     exit
 
 
-  :no2
+  :no11
     java -jar BuildTools.jar --rev 1.18
     ren spigot-1.18.jar 1.18.jar
     java -Xms1024M -Xmx1024M -jar 1.18.jar nogui
@@ -257,7 +633,7 @@ if /i {%ANSWER%}=={4} (EXIT)
 
 
 
-  :no3
+  :no12
     java -jar BuildTools.jar --rev 1.19
     ren spigot-1.19.jar 1.19.jar
     java -Xms1024M -Xmx1024M -jar 1.19.jar nogui
